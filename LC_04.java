b305fbb02677a510308381ef03c5bb932ac5cc90
wuxin0011 <2191377759@qq.com>
2024-11-14 07:03:12 +0800

    update

---
+++ LC_04.java
@@ -0,0 +1,16 @@
+import java.util.*; 
+import java.io.*; 
+
+/*
+@testcase-start
+
+@testcase-end
+*/
+
+public class LC_04 {
+    public static void main(String[] args) {
+
+         // _LCUtil.run(LC_04.class,"null","../in/leetcode/LC_04-in.txt");
+         _LCUtil.run(LC_04.class,"null","__TEST_CASE__");
+	}
+}
