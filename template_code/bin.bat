7728b01158f26ac94a503249c2b9faac4bde1e6c
wuxin0011 <2191377759@qq.com>
2026-01-22 09:17:35 +0800

    update

--- template_code/bin.bat
+++ template_code/bin.bat
@@ -1,1 +1,1 @@
-g++ -D__IS_LOCAL__=1 -D__IS_LEETCODE__=1 "%1.cpp" -o "%1" && "%1" < "%1.txt" > "%1-out.txt"
+g++ -D__IS_LOCAL__=1 -D__IS_LEETCODE__=1 -D__IS_DEBUG__=1 -std=c++2a -g -fsanitize=address -fsanitize=undefined -fno-omit-frame-pointer -O1 -Wall -Wextra -pedantic "%1.cpp" -o "%1" && "%1" < "%1.in" > "%1.out"
